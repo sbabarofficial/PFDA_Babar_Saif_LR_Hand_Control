f49d8ed1d60ac8f6acf69b8ca7db94bfe3799fe7
sbabarofficial <sbabar.official@gmail.com>
2026-05-04 19:25:24 -0500

    creates a cube made up of a nurbs curve

--- src/control.py
+++ src/control.py
@@ -14,5 +14,31 @@ def create_cube_control(name="cube_ctrl", size=1.0):
         str: The name of the created control curve.
     """
 
-   
+    h = size * 0.5  
 
+    
+    points = [
+        (-h, -h, -h), (h, -h, -h),
+        (h, -h, -h), (h, -h, h),
+        (h, -h, h), (-h, -h, h),
+        (-h, -h, h), (-h, -h, -h),
+
+        (-h, -h, -h), (-h, h, -h),
+        (h, h, -h), (h, -h, -h),
+
+        (h, -h, h), (h, h, h),
+        (h, h, -h), (h, h, h),
+        (-h, h, h), (-h, -h, h),
+        (-h, h, h), (-h, h, -h)
+    ]
+
+    
+    cube = cmds.curve(name=name, degree=1, point=points)
+
+    
+    cmds.xform(cube, worldSpace=True, pivots=(0, 0, 0))
+
+    
+    cmds.makeIdentity(cube, apply=True, translate=True, rotate=True, scale=True)
+
+    return cube
