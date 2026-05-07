7077b6dcfd5d21fb646b745afab75c9ad2f68f67
sbabarofficial <sbabar.official@gmail.com>
2026-05-07 18:53:35 -0500

    created  the actual attributes that will be used

--- src/attributes.py
+++ src/attributes.py
@@ -2,5 +2,16 @@ import maya.cmds as cmds
 
 
 def add_hand_attributes(control):
-    
-  
+   
+    fingers = ["fist", "fore", "mid", "ring", "pinky", "thumb"]
+    categories = ["clench", "twist", "spread"]
+
+    created = []
+
+   
+    if not cmds.attributeQuery("Fingers", node=control, exists=True):
+        cmds.addAttr(control, longName="Fingers", attributeType="enum", enumName="-----")
+        cmds.setAttr(f"{control}.Fingers", lock=True)
+        created.append(f"{control}.Fingers")
+
+   
