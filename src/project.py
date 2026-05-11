67c27dc64f456f3d8accf9307ed72c3b51ea538d
sbabarofficial <sbabar.official@gmail.com>
2026-05-11 17:31:18 -0500

    created project.py

---
+++ src/project.py
@@ -0,0 +1,17 @@
+from rig_con import main as run_rig_tool
+
+def main():
+    print("Launching LR Hand Control Generator...")
+    run_rig_tool()
+
+def helper_1():
+    return "Helper 1 executed"
+
+def helper_2():
+    return "Helper 2 executed"
+
+def helper_3():
+    return "Helper 3 executed"
+
+if __name__ == "__main__":
+    main()
